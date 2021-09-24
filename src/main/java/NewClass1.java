3e89032fc794fd190fca4728a7d95cac1abcb132
JosephPM93 <josias.jjpm49@gmail.com>
2021-09-24 14:32:40 -0600

    segunda modificacion para conflictos

--- src/main/java/NewClass1.java
+++ src/main/java/NewClass1.java
@@ -11,5 +11,9 @@
 public class NewClass1 {
     public static void main(String[] args) {
         System.out.println("Modificando en la rama master para visualizar error en merge");
+        
+        
+        
+        System.out.println("Hellooooo");
     }
 }
