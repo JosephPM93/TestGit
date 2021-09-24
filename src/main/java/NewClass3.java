3d4a7d08fbe2c3706e9be688046a96a7178d540b
JosephPM93 <josias.jjpm49@gmail.com>
2021-09-24 14:29:14 -0600

    Modificando en ramas

---
+++ src/main/java/NewClass3.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author José Padilla
+ */
+public class NewClass3 {
+    public static void main(String[] args) {
+        System.out.println("Otra claseeeeeee");
+    }
+}
